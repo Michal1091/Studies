4691062d9ecb93312ae43bace5567c9fb7f7f137
Michal1091 <michalos109@gmail.com>
2026-05-11 20:35:52 +0200

    task_2 - dodawanie plików przez konsolę

---
+++ math_project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace math_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
